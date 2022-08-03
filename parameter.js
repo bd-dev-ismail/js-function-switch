54a67f001c87112db56dbfa761465397c78cd329
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-03 22:59:50 +0600

    added parameter

--- parameter.js
+++ parameter.js
@@ -1,5 +1,22 @@
-function buyApple(money){
-    console.log('Give me Apple');
-    console.log(money);
+// function buyApple(money){
+//     console.log('Give me Apple');
+//     console.log(money);
+// }
+// buyApple(500);
+function orange(tk){
+    
+    console.log('Give me orange');
+    console.log('tk dicen:', tk);
 }
-buyApple(500);
+var money = 25;
+// orange(money);
+function add(num1, num2){
+    console.log('added number: ', num1, num2);
+}
+// add(120, 520);
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d +e;
+    console.log(sum);
+}
+sum(15, 25, 35, 45, 55);
