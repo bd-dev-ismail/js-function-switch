748c012bf91b09bb37d9f69240e1b148d112c9bc
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-03 22:28:29 +0600

    function delcareing

---
+++ function.js
@@ -0,0 +1,8 @@
+function startFan(){
+    console.log('Stand up');
+    console.log('Click to the swtich');
+    console.log('& come there');
+}
+startFan();
+startFan();
+startFan();
---
+++ parameter.js
@@ -0,0 +1,5 @@
+function buyApple(money){
+    console.log('Give me Apple');
+    console.log(money);
+}
+buyApple(500);
