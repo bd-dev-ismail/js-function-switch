5cd5736ee0f93108a3022039980d0f3852365286
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-04 05:31:41 +0600

    added

--- parameter.js
+++ parameter.js
@@ -1,22 +1,33 @@
-// function buyApple(money){
-//     console.log('Give me Apple');
-//     console.log(money);
-// }
-// buyApple(500);
-function orange(tk){
+// // function buyApple(money){
+// //     console.log('Give me Apple');
+// //     console.log(money);
+// // }
+// // buyApple(500);
+// function orange(tk){
     
-    console.log('Give me orange');
-    console.log('tk dicen:', tk);
+//     console.log('Give me orange');
+//     console.log('tk dicen:', tk);
+// }
+// var money = 25;
+// // orange(money);
+// function add(num1, num2){
+//     console.log('added number: ', num1, num2);
+// }
+// // add(120, 520);
+// function sum(a, b, c, d, e){
+//     console.log(a, b, c, d, e);
+//     var sum = a + b + c + d +e;
+//     console.log(sum);
+// }
+// sum(15, 25, 35, 45, 55);
+function student(stu1, stu2){
+    console.log('The student name is:' ,stu1, stu2);
+    console.log('they are not coming school timely');
 }
-var money = 25;
-// orange(money);
-function add(num1, num2){
-    console.log('added number: ', num1, num2);
-}
-// add(120, 520);
+student('rakib', 'raihan');
 function sum(a, b, c, d, e){
     console.log(a, b, c, d, e);
-    var sum = a + b + c + d +e;
+    var sum = a + b + c + d + e;
     console.log(sum);
 }
-sum(15, 25, 35, 45, 55);
+sum(20, 35, 45, 55, 50);
